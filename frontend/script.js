fdf9ef37c2c68346165dcf628e4665fabdb02647
SidereaH <hutornoyaa@gmail.com>
2024-04-29 19:07:07 +0300

    Страница с просмотром команд

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-
+const reloadButton = document.getElementById('reload');
 class Team{
     constructor(email, capitan, password, members){
         this.teamEmail = email
@@ -33,16 +33,26 @@ loginBut.onclick = function() {
             let passwordTeam = Team.instanes[team].getPass()
             console.log(emailTeam, passwordTeam)
             if(email === emailTeam && passwordTeam === password){
-                succesful = true
+                window.location.href = 'index2.html';
+                return succesful = true
+
             }
-            
         }
         if(succesful === true){
-            console.log("succesful")
+            alert("succesful")
         }
         else{
-            console.log("Неверный логин или пароль в итерации")
+            alert("Неверный логин или пароль")
         }
+    }
+    else if(isEmpty(email) === true && isEmpty(password) === true ){
+        alert("Введите логин и пароль")
+    }
+    else if(isEmpty(email) === true ){
+        alert("Введите логин")
+    }
+    else{
+        alert("Введите пароль")
     }
 }
 
@@ -51,3 +61,52 @@ function isEmpty(str) {
       return true      
     else return false
   }
+
+
+
+function render() {
+    listElement.innerHTML = ''
+    if(notes.length === 0){
+        listElement.innerHTML = '<p>Нет элементов</p>'
+    }
+    for (let i = 0; i < notes.length; i++){
+
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
+
+    }
+
+}
+render()
+
+
+
+
+reloadButton.onclick = function () {
+    if(inputElement.value.length === 0){
+        return
+    }
+    const newNote = {
+        title: inputElement.value,
+        completed: false
+    }
+
+    notes.push(newNote)
+    render()
+    inputElement.value = ''
+}
+function getNoteTemplate(note, index){
+    return  `
+    <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+        >
+        <span class = "${note.completed ? 'text-decoration-line-through':''}">${note.title}</span>
+        <span>
+
+         <span class="btn btn-small btn-${note.completed ? 'warning': 'success'}" data-index = "${index}" data-type = "toggle">&check;</span>
+
+          <span class="btn btn-small btn-danger" data-index = "${index}" data-type = "remove">&times;</span>
+
+        </span>
+
+      </li>`
+}
